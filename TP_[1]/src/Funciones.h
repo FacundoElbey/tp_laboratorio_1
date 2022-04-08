74188ebaf2f55b7a2fdc64489bac730a8d40c74a
FacundoElbey <elbeyfacundo@gmail.com>
2022-04-08 09:47:03 -0300

    primer commit

---
+++ TP_[1]/src/Funciones.h
@@ -0,0 +1,21 @@
+/*
+ * Funciones.h
+ *
+ *  Created on: 7 abr. 2022
+ *      Author: felbey
+ */
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int calcularPrecioDebito(float, float*);
+int calcularPrecioCredito(float, float*);
+int calcularPrecioBitcoin(float,float, float*);
+int calcularPrecioUnitario(float, float, float*);
+int calcularDiferenciaPrecios(float, float, float*);
+
+
+#endif /* FUNCIONES_H_ */
